7a7dbe6239cac02d3dd61a24c158526146f4c263
Rednexie <126991413+Rednexie@users.noreply.github.com>
2025-01-23 16:17:25 +0300

    boilerplate
    setup electron boilerplate

---
+++ src/main/index.js
@@ -0,0 +1,35 @@
+import { app, BrowserWindow, ipcMain } from "electron";
+import { attachTitlebarToWindow, setupTitlebar } from 'custom-electron-titlebar/main'
+
+let mainWindow;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        titleBarOverlay: false,
+        titleBarStyle: 'hidden',
+        webPreferences: {
+        preload: path.join(__dirname, 'preload.js'), // Optional
+        nodeIntegration: true,
+        contextIsolation: true
+        },
+    });
+
+
+    // sunucu ve client eventler tetikleyerek ve arg göndererek iletişim kuruyo
+    
+    ipcMain('message-from-renderer', (event, args) => {
+        console.log('event triggered from client')
+        event.reply('message-from-main', [])
+
+    })
+
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+});
+
+// main process: nodejs sunucusu
+// os ve fs ile etkileşimleri yönetir
---
+++ src/main/preload.js
@@ -0,0 +1,20 @@
+const { Titlebar, TitlebarColor } = require('custom-electron-titlebar')
+const { app, contextBridge } = require('electron')
+
+
+// electron main process ve renderer arasındaki köprüdür.
+// tarayıcı javascript ortamına server'dan nelerin yükleneceğini belirtir
+
+
+contextBridge.exposeInMainWorld('Electron', {
+    titlebar: () => {
+        return new Titlebar({backgroundColor: TitlebarColor.fromHex('#6A4C30')})
+    },
+    sendMessage: (channel, data) => {
+        ipcRenderer.send(channel, data);
+    },
+    receiveMessage: (channel, func) => {
+        ipcRenderer.on(channel, (event, ...args) => func(...args));
+    }
+    
+})
